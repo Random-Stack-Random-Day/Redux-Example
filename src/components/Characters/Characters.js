b8b477b40c90923cd7cadb6bc553d2402272df5b
Donavin Northrup <donavin@dnorthrupga.com>
2018-04-14 00:50:19 -0400

    UpdatedCards

--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -1,29 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Grid from 'material-ui/Grid';
+import { compose } from 'redux';
 import { connect } from 'react-redux';
+import Paper from 'material-ui/Paper';
+
 import Form from './CharacterForm';
 import ComplexCards from '../UI/Cards/ComplexCards';
 
-const Characters = (props) => {
-    return (
-        <div>
-            {console.log(props.characters, "char")}
-            {Object.keys(props.characters)
-                .map((k,index) => {
-                    // return <li key={index}>{props.characters[k].name}</li>
-                    return <ComplexCards name = {props.characters[k].name}
-                    charClass={props.characters[k].class}
-                    level={props.characters[k].level} 
-                    key={index}
-                    />
-                })
-            }
-            <Form />
-        </div>
-    );
-};
 
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+
+class Characters extends Component {
+  render() {
+    const { classes } = this.props;
+        return (
+            <div className={classes.root}>
+            <Grid container spacing={24}>
+                
+                    {Object.keys(this.props.characters)
+                                    .map((k,index) => {
+                                        // return <li key={index}>{props.characters[k].name}</li>
+                                        return <Grid item xs={6} sm={3} key={index}> <ComplexCards name = {this.props.characters[k].name}
+                                        charClass={this.props.characters[k].class}
+                                        level={this.props.characters[k].level} 
+                                        
+                                        />
+                                        </Grid>
+                                    })
+                                }
+            </Grid>
+            </div>
+        );
+    }
+}
+
+Characters.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 const mapStateToProps = state => {
     return { characters: state.characters };
 }
 
-export default connect(mapStateToProps)(Characters);
+export default compose(connect(mapStateToProps), withStyles(styles))(Characters)
